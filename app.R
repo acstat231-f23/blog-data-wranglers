b1ebadd9ae2c95da9275780abbab7f6eab9eebc4
Jluetke <144145782+Jluetke@users.noreply.github.com>
2023-12-12 21:26:47 -0500

    Comments

--- app.R
+++ app.R
@@ -3,31 +3,34 @@ library(leaflet)
 library(dplyr)
 
 ui <- fluidPage(
-  titlePanel("Sports Team Analysis"),
-  selectInput("league", "Choose a League:", choices = c("NFL", "NHL", "NBA", "MLB")),
+  titlePanel("Sports Team Analysis"),  # Title of the app
+  selectInput("league", "Choose a League:", choices = c("NFL", "NHL", "NBA", "MLB")),  # Dropdown for selecting a league
   selectInput("variable", "Select Variable:", choices = c("Payroll" = "payroll", 
                                                           "Win Percentage" = "win_percentage", 
-                                                          "Value" = "value")),
+                                                          "Value" = "value")),  # Dropdown for selecting a variable to display
   leafletOutput("map")
 )
 
 server <- function(input, output) {
   
+  # Read data
   data <- read.csv("wrangled_data.csv")
   
+  # Rename Variables for popups
   variableNames <- c(payroll = "Payroll", win_percentage = "Win Percentage", value = "Value")
   
   filteredData <- reactive({
-    data %>% filter(league == input$league)
+    data %>% filter(league == input$league)  # Filter data by selected league
   })
   
+  # Show the map
   output$map <- renderLeaflet({
     leaflet(filteredData()) %>%
       addTiles() %>%
       addCircleMarkers(
-        lng = ~longitude, lat = ~latitude,
-        radius = ~scales::rescale(get(input$variable), to = c(5, 15)),
-        popup = ~paste(team, "<br>", variableNames[[input$variable]], ": ", get(input$variable))
+        lng = ~longitude, lat = ~latitude,  # Set marker positions based on longitude and latitude
+        radius = ~scales::rescale(get(input$variable), to = c(5, 15)),  # Adjust marker size based on selected variable
+        popup = ~paste(team, "<br>", variableNames[[input$variable]], ": ", get(input$variable))  # Add popups with team info
       )
   })
 }
